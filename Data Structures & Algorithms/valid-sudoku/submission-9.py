574f7b505d91ac6c650022b6ba54594b3643d362
Nana Kojo Atta-Benyah <124890914+Nana-Kojo801@users.noreply.github.com>
2026-09-15 11:13:15 +0000

    Add: valid-sudoku - submission-9

---
+++ Data Structures & Algorithms/valid-sudoku/submission-9.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(0, len(board) - 3, 3):
+            for j in range(0, len(board[i]) - 3, 3):
+                chars = board[i][j:j+3] + board[i+1][j:j+3] + board[i+2][j:j+3]
+                new_chars = [c for c in chars if c != "."]
+
+                if len(set(new_chars)) != len(new_chars):
+                    return False
+        
+        for i in range(len(board)):
+            chars = [board[j][i] for j in range(len(board))]
+            new_chars = [c for c in chars if c != "."]
+
+            if len(set(new_chars)) != len(new_chars):
+                return False
+
+        for i in range(len(board)):
+            chars = board[i]
+            new_chars = [c for c in chars if c != "."]
+
+            if len(set(new_chars)) != len(new_chars):
+                return False
+        return True
+
