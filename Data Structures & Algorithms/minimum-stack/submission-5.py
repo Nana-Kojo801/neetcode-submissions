16b9d17a674acff588975ad5008afb8a809805a8
Nana Kojo Atta-Benyah <124890914+Nana-Kojo801@users.noreply.github.com>
2026-09-17 20:13:09 +0000

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_values = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_values:
+            self.min_values.append(val)
+        else:
+            if val <= self.min_values[-1]:
+                self.min_values.append(val)
+    
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if self.getMin() == val:
+            self.min_values.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_values[-1]
